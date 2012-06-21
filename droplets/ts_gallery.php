d69ce4d404dac76da949708bc58c9d74a3591789
hertsch <ralf.hertsch@phpmanufaktur.de>
2012-06-21 12:55:07 +0200

    Droplet ts_gallery is now autoloading Lytebox

--- droplets/ts_gallery.php
+++ droplets/ts_gallery.php
@@ -8,6 +8,13 @@
  * @copyright 2011 - 2012
  * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
  */
+// load the ts_gallery.preset with LibraryAdmin
+include_once WB_PATH.'/modules/libraryadmin/include.php';
+$new_page = includePreset( $wb_page_data, 'libraryadmin', 'ts_gallery', 'ts_gallery', NULL, false, NULL, NULL );
+if ( !empty($new_page) ) {
+    $wb_page_data = $new_page;
+}
+
 if (file_exists(WB_PATH.'/modules/ts_gallery/class.frontend.php')) {
 	require_once(WB_PATH.'/modules/ts_gallery/class.frontend.php');
 	$gallery = new tsgFrontend();
